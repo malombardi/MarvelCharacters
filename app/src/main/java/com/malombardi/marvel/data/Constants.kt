b71c9b024ca0e2f41e6043466a1bdf0715d9f792
Mauro Lombardi <mauro.lombardi@gmail.com>
2022-02-11 18:45:54 -0300

    bio changed

--- app/src/main/java/com/malombardi/marvel/data/Constants.kt
+++ app/src/main/java/com/malombardi/marvel/data/Constants.kt
@@ -3,7 +3,7 @@ package com.malombardi.marvel.data
 object Constants {
     const val IMAGE_DEFAULT_SIZE = "portrait_xlarge"
     const val IMAGE_BIG_SIZE = "portrait_uncanny"
-    const val URL_BIO_TYPE = "wiki"
+    const val URL_BIO_TYPE = "detail"
     const val CHARACTER_ID = "character_id"
     const val CHARACTER_ARGS = "character"
 }
