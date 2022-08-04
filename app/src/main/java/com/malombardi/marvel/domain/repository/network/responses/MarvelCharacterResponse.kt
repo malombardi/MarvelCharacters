7df0f1e3dad7220c54c1783be1b1ec7863b5c956
Mauro Lombardi <mauro.lombardi@gmail.com>
2022-08-04 13:47:43 -0300

    removed unused code

--- app/src/main/java/com/malombardi/marvel/domain/repository/network/responses/MarvelCharacterResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.malombardi.marvel.domain.repository.network.responses
-
-data class MarvelCharacterResponse(
-    val data: CharacterData,
-)
-
-data class CharacterData(
-    val results: List<MarvelCharacter>
-)
--- app/src/main/java/com/malombardi/marvel/domain/repository/network/responses/Thumbnail.kt
+++
@@ -1,6 +0,0 @@
-package com.malombardi.marvel.domain.repository.network.responses
-
-data class Thumbnail(
-    val extension: String?,
-    val path: String?
-)
